e78a20db4ac25cef4a7a6da282079751068a17ed
Martin Chapman <contact@martinchapman.co.uk>
2025-04-30 18:16:49 +0100

    Practice: Exception handling

--- drunkard.py
+++ drunkard.py
@@ -1,7 +1,17 @@
 import random
 
-width = int(input('Enter width of space: '))
-height = int(input('Enter height of space: '))
+# Because we need two integers, keep looping until the user gives us those two integers (rather than, say, one or more strings).
+while True:
+    try:
+        # Cast each input as an integer, and if either fails, the exception will simply bring us back to the top of the loop.
+        width = int(input('Enter width of space: '))
+        height = int(input('Enter height of space: '))
+        # If we succeed in converting two integers from the input, break from (exit) the loop.
+        break
+    # Exceptions can have different 'types'. This is the type of exception we know is 'thrown' when trying to convert a non-integer string to an integer, so we explicitly listen for it.
+    except ValueError:
+         # We need to list something for the exception to adhere to Python's rules, but we don't need to do anything (we'll go back to the top of the loop automatically), so just add pass, which does nothing.
+        pass
 x = 0
 y = 0
 path = []
